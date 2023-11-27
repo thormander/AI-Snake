2b12c4880273a60ccdb9176a63c66bb4939c83d5
thormander <71967190+thormander@users.noreply.github.com>
2023-11-27 13:59:54 -0600

    basic class and constructors

---
+++ snake.py
@@ -0,0 +1,32 @@
+import pygame
+import random
+
+pygame.init() # initilize modules
+
+class Snake:
+    # pass window size of game
+    def __init__(self,h=600,w=600):
+        self.h = h
+        self.w = w
+
+        # window of game
+
+        # initial game state
+    
+    def play(self):
+        pass
+
+    
+    
+if __name__ == '__main__':
+    game = Snake()
+
+    # Run game (ie. infinite loop until game over)
+    while True:
+        game.play()
+        
+        # game over condition
+    
+    pygame.quit()
+
+
