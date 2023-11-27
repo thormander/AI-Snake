0bf5509edca4a80f833c75410210dcdc16de86e5
thormander <71967190+thormander@users.noreply.github.com>
2023-11-27 16:06:43 -0600

    Mostly done with snake game

--- snake.py
+++ snake.py
@@ -8,13 +8,17 @@ pygame.init() # initilize modules
 
 # CONSTANTS
 SIZE = 20
-SPEED = 30
+SPEED = 10
 
 # COLOR CONSTANTS
 BLACK = (0,0,0)
+WHITE = (255,255,255)
 FOOD = (255,0,0)
 SNAKE_COLOR1 = (0,100,255)
 SNAKE_COLOR2 = (0,20,255)
+
+# FONTS
+font = pygame.font.SysFont('arial', 25, bold=False, italic=False)
 
 
 class Direction(Enum):
@@ -38,7 +42,7 @@ class SnakeMain:
         self.clock = pygame.time.Clock()
 
         # initial game state
-        self.dir = Direction.LEFT
+        self.direction = Direction.LEFT
         self.head = Point(self.w/2,self.h/2)
         self.snake = [self.head, Point(self.head.x-SIZE,self.head.y),Point(self.head.x-(2*SIZE),self.head.y)]
 
@@ -57,20 +61,72 @@ class SnakeMain:
 
     def play(self):
         # Get user input
+        for event in pygame.event.get():
+            # quit
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+            # keystrokes
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.direction = Direction.LEFT
+                elif event.key == pygame.K_RIGHT:
+                    self.direction = Direction.RIGHT
+                elif event.key == pygame.K_UP:
+                    self.direction = Direction.UP
+                elif event.key == pygame.K_DOWN:
+                    self.direction = Direction.DOWN
 
         # move snake
+        self.moveSnake(self.direction) # update head
+        self.snake.insert(0,self.head) 
 
-        # game over?
+        # game over? (hit itself or border)
+        gameOver = False
+        if self.collision():
+            gameOver = True
+            return gameOver, self.score
 
         # generate new food
+        if self.head == self.food:
+            self.score += 1
+            self.generateFood()
+        else:
+            self.snake.pop()
 
         # update
         self.updateUI()
         self.clock.tick(SPEED)
 
         # return score after game over
-        gameOver = False
         return gameOver, self.score
+
+    def collision(self):
+        # hit boundary
+        if self.head.x > self.w - SIZE or self.head.x < 0 or self.head.y > self.h - SIZE or self.head.y < 0:
+            return True
+
+        # hit self
+        if self.head in self.snake[1:]: # do not include head
+            return True
+        
+        return False
+
+    def moveSnake(self,direction):
+        x = self.head.x
+        y = self.head.y
+
+        if direction == Direction.RIGHT:
+            x += SIZE
+        elif direction == Direction.LEFT:
+            x -= SIZE
+        elif direction == Direction.UP:
+            y -= SIZE
+        elif direction == Direction.DOWN:
+            y += SIZE
+        
+        self.head = Point(x,y)
 
     # drawing the items for the game
     def updateUI(self):
@@ -78,12 +134,15 @@ class SnakeMain:
 
         # draw snake
         for point in self.snake:
-            pygame.draw(self.display,SNAKE_COLOR1,pygame.Rect(point.x,point.y,SIZE,SIZE))
-            pygame.draw(self.display,SNAKE_COLOR2,pygame.Rect(point.x+4,point+4.y,12,12))
+            pygame.draw.rect(self.display,SNAKE_COLOR1,pygame.Rect(point.x,point.y,SIZE,SIZE))
+            pygame.draw.rect(self.display,SNAKE_COLOR2,pygame.Rect(point.x+4,point.y+4,12,12))
         
         # draw food
-        pygame.draw(self.display, FOOD, pygame.Rect(self.food.x,self.food.y,SIZE,SIZE))
-    
+        pygame.draw.rect(self.display, FOOD, pygame.Rect(self.food.x,self.food.y,SIZE,SIZE))
+
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0,0])
+        pygame.display.flip() # updates the screen user sees
 
     
 if __name__ == '__main__':
@@ -95,6 +154,7 @@ if __name__ == '__main__':
         
         # game over condition
         if gameOver == True:
+            pygame.time.wait(5000)  # Wait for 5 seconds
             break
             
     print('Your score: ', score)
